546bc7a2d3f49e6dfad161307a83afeffc6976ff
Nadith Dhanula <116380737+nadith-pro@users.noreply.github.com>
2023-06-21 23:55:42 +0400

    @nadithpro

--- script.js
+++ script.js
@@ -3,12 +3,12 @@ import { check, sleep } from 'k6';
 export let options = {
   stages: [
     { duration: '15s', target: 10000 },
-    { duration: '300s', target: 10000 },
+    { duration: '30s', target: 10000 },
     { duration: '15s', target: 0 },
   ],
 };
 export default function() {
-  let res = http.get('https://tv.nadith.pro/');
+  let res = http.get('https://firemovieshub.ml/');
   check(res, { 'status was 200': r => r.status == 200 });
   sleep(1);
 }
