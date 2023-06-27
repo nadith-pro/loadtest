54678a97c1bf7ec1533e242a119758616754dc30
nadith-pro <116380737+nadith-pro@users.noreply.github.com>
2023-06-27 15:28:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 export let options = {
   stages: [
-    { duration: '15s', target: 25000 },
-    { duration: '40s', target: 25000 },
-    { duration: '15s', target: 0 },
+    { duration: '300s', target: 20 },
+    { duration: '300s', target: 20 },
+    { duration: '500s', target: 0 },
   ],
 };
 export default function() {
-  let res = http.get('https://www.cineru.lk/');
+  let res = http.get('https://www.tvzonelk.com/');
   check(res, { 'status was 200': r => r.status == 200 });
   sleep(1);
 }
